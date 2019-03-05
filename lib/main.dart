dbd52f9c1e8fd26a6aa338bba629ebc6c409a72e
yaroslav <yaroslavtihonov1@gmail.com>
2019-03-06 00:31:44 +0200

    ddd

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new _RandomColorState();
+  }
+}
+
+class RandomColor extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new _RandomColorState();
+  }
+}
+
+class _RandomColorState extends State<MyApp> {
+  final Random _random = Random();
+  Color _color = Color(0xFFFFFFFF);
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        backgroundColor: _color,
+        appBar: AppBar(
+          title: Text("Color Tap"),
+        ),
+        body: InkWell(
+          onTap: changeColor,
+          child: Center(
+            child: new Container(
+                child: Text(
+              "Hey There",
+              style: TextStyle(fontSize: 18),
+            )),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeColor() {
+    setState(() {
+      _color = Color.fromARGB(
+        _random.nextInt(255),
+        _random.nextInt(255),
+        _random.nextInt(255),
+        _random.nextInt(255),
+      );
+    });
+  }
+}
